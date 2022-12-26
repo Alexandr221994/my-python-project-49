5f7bfbddc03c1fb2547e1f8104110b39147c65f8
Alexandr Mityushkin <alexandrmityushkin@MacBook-Pro-Alexandr.local>
2022-12-26 18:52:12 +0300

    start version

---
+++ brain_games/games/prime_game.py
@@ -0,0 +1,20 @@
+from random import randint
+
+OUTPUT_TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def search_prime_number(number: int):
+    count = 0
+    for item in range(1, number + 1):
+        if number % item == 0:
+            count += 1
+    return count == 2
+
+
+def get_game():
+    question = randint(1, 101)
+    if search_prime_number(question):
+        correct_answer = 'yes'
+    else:
+        correct_answer = 'no'
+    return question, str(correct_answer)
