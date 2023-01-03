bcddf48873fae67ceec79ff63c8aa14da72c8cd3
Alexandr Mityushkin <alexandrmityushkin@MacBook-Pro-Alexandr.local>
2023-01-03 20:09:22 +0300

    add arg_parser.py and brain_game.py, update pyproject.toml and rm games scripts

---
+++ brain_games/arg_parser.py
@@ -0,0 +1,19 @@
+import argparse
+from brain_games.games import calc_game, even_game, gcd_game, prime_game, progression_game
+
+GAMES = {
+    'calc': calc_game,
+    'even': even_game,
+    'gcd': gcd_game,
+    'prime': prime_game,
+    'progression': progression_game
+}
+
+
+def args_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-gn", "--gamename",
+                        help="by adding a certain argument (calc, even, gcd, "
+                             "progression, prime) you get a selected game")
+    args = parser.parse_args()
+    return GAMES[args.gamename]
--- brain_games/scripts/brain_game.py
+++ brain_games/scripts/brain_game.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 from brain_games.game_starter import run_game
-from brain_games.games import gcd_game
+from brain_games import arg_parser
 
 
 def main():
-    run_game(gcd_game)
+    run_game(arg_parser.args_parser())
 
 
 if __name__ == '__main__':
